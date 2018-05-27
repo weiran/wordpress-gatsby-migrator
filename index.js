a8d5e0b3fbcd79026de3ade81b86a2b14ba107f5
Weiran Zhang <weiran@zhang.me.uk>
2018-05-27 12:43:59 +0100

    Extracted parsing to another file

--- index.js
+++ index.js
@@ -1,66 +1,52 @@
 const fs = require('fs-extra')
-const path = require('path')
-const feedRead = require('davefeedread')
-const TurndownService = require('turndown')
-const turndownService = new TurndownService({
-    headingStyle: 'atx',
-    codeBlockStyle: 'fenced'
-})
-const cheerio = require('cheerio')
 const fetch = require('node-fetch')
-const uuid = require('uuid/v4') // v4 generates random UUIDs
-const url = require('url')
+const args = require('minimist')(process.argv.slice(2))
 
-const parseBlog = async (file) => {
-    const feed = await parseFeed(file)
+const parse = require('./parse.js')
 
-    // Filter for only blog posts
-    var items = feed.items.filter((item, index, array) => item['wp:post_type']['#'] === 'post')
-    
-    // Map to new object type
-    items = items.map(item => {
-        if (item['wp:post_type']['#'] !== 'post') {
-            return
-        }
+const writeBlog = async (blog, rootPath) => {
+    if (!rootPath.endsWith('/')) {
+        rootPath = rootPath + '/'
+    }
 
-        const mappedItem = {
-            'title': item.title,
-            'date': item.date,
-            'content': item['content:encoded']['#'],
-            'categories': item.categories,
-            'slug': item['wp:post_name']['#']
-        }
+    blog.forEach(async post => {
+        const postPath = `${__dirname}/${rootPath}${post.slug}`
+        await fs.ensureDir(postPath)
 
-        // Add passthroughUrl if exists
-        const postMeta = item['wp:postmeta']
-        if (postMeta) {
-            const metaKey = postMeta['wp:meta_key']['#']
-            if (metaKey == "passthrough_url") {
-                mappedItem.passthroughUrl = postMeta['wp:meta_value']['#']
-            }
-        }
+        // // fetch images
+        // const postElements = cheerio.load(post.content)
+        // const imagesElements = postElements('img')
+        // imagesElements.each(async (index, item) => {
+        //     try {
+        //         const imageUrl = item.attribs['src']
+        //         const imageExtension = path.extname(url.parse(imageUrl).pathname)
+        //         const imageName = uuid()
+        //         const imagePath = `${postPath}/${imageName}${imageExtension}`
 
-        return mappedItem
-    })
+        //         const imageResponse = await fetch(imageUrl)
+        //         const writeStream = fs.createWriteStream(imagePath)
+        //         imageResponse.body.pipe(writeStream)
+        //         await streamAsync(writeStream)
 
-    // Add Markdown conversion
-    items = items.map(item => {
-        item.markdownContent = turndownService.turndown(item.content)
-        return item
-    })
+        //         // TODO: not working, suspect await isn't working somewhere up
+        //         post.content = post.content.replace(imageUrl, `${imageName}${imageExtension}`)
+        //         post.markdownContent = post.markdownContent.replace(imageUrl, `${imageName}${imageExtension}`)
+        //     }
+        //     catch (error) {
+        //         console.error(error)
+        //     }
+        // })
 
-    return items
-}
+        // create index.md contents
+        const fileContents = 
+`---
+title: ${post.title}
+date: "${post.date}"
+---
 
-const parseFeed = (file) => {
-    return new Promise((resolve, reject) => {
-        feedRead.parseString(file, undefined, (error, result) => {
-            if (error) {
-                reject(error)
-            } else {
-                resolve(result)
-            }
-        })
+${post.markdownContent}`
+
+        await fs.outputFile(`${postPath}/index.md`, fileContents)
     })
 }
 
@@ -78,55 +64,11 @@ const streamAsync = (stream) => {
     })
 }
 
-const writeBlog = async (blog, rootPath) => {
-    if (!rootPath.endsWith('/')) {
-        rootPath = rootPath + '/'
-    }
-
-    blog.forEach(async post => {
-        const postPath = `${__dirname}/${rootPath}${post.slug}`
-        await fs.ensureDir(postPath)
-
-        // fetch images
-        const postElements = cheerio.load(post.content)
-        const imagesElements = postElements('img')
-        imagesElements.each(async (index, item) => {
-            try {
-                const imageUrl = item.attribs['src']
-                const imageExtension = path.extname(url.parse(imageUrl).pathname)
-                const imageName = uuid()
-                const imagePath = `${postPath}/${imageName}${imageExtension}`
-
-                const imageResponse = await fetch(imageUrl)
-                const writeStream = fs.createWriteStream(imagePath)
-                imageResponse.body.pipe(writeStream)
-                await streamAsync(writeStream)
-
-                // TODO: not working, suspect await isn't working somewhere up
-                post.content = post.content.replace(imageUrl, `${imageName}${imageExtension}`)
-                post.markdownContent = post.markdownContent.replace(imageUrl, `${imageName}${imageExtension}`)
-            }
-            catch (error) {
-                console.error(error)
-            }
-        })
-
-        // create index.md contents
-        const fileContents = 
-`---
-title: ${post.title}
-date: "${post.date}"
----
-
-${post.markdownContent}`
-
-        await fs.outputFile(`${postPath}/index.md`, fileContents)
-    })
-}
-
 const runner = async () => {
-    const file =  fs.readFileSync(__dirname + '/squarespace.xml', 'utf8')
-    const blog = await parseBlog(file)
+    // const file =  fs.readFileSync(__dirname + '/squarespace.xml', 'utf8')
+    const inputFile = args._[0]
+    const file = fs.readFileSync(inputFile, 'utf8')
+    const blog = await parse.parse(file)
     await writeBlog(blog, 'blogs')
 }
 
---
+++ parse.js
@@ -0,0 +1,85 @@
+const feedRead = require('davefeedread')
+const TurndownService = require('turndown')
+const turndownService = new TurndownService({
+    headingStyle: 'atx',
+    codeBlockStyle: 'fenced'
+})
+const cheerio = require('cheerio')
+const uuid = require('uuid/v4') // v4 generates random UUIDs
+const url = require('url')
+const path = require('path')
+
+const parse = async (file) => {
+    const feed = await parseFeed(file)
+
+    // Filter for only blog posts
+    var items = feed.items.filter((item, index, array) => item['wp:post_type']['#'] === 'post')
+    
+    // Map to new object type
+    items = items.map(item => {
+        if (item['wp:post_type']['#'] !== 'post') {
+            return
+        }
+
+        const mappedItem = {
+            'title': item.title,
+            'date': item.date,
+            'content': item['content:encoded']['#'],
+            'categories': item.categories,
+            'slug': item['wp:post_name']['#']
+        }
+
+        // Add passthroughUrl if exists
+        const postMeta = item['wp:postmeta']
+        if (postMeta) {
+            const metaKey = postMeta['wp:meta_key']['#']
+            if (metaKey == "passthrough_url") {
+                mappedItem.passthroughUrl = postMeta['wp:meta_value']['#']
+            }
+        }
+
+        // Add images array
+        const postElements = cheerio.load(mappedItem.content)
+        const imagesElements = postElements('img')
+        const images = imagesElements.map((index, item) => {
+            const imageName = uuid()
+            const imageUrl = item.attribs['src']
+            const imageExtension = path.extname(url.parse(imageUrl).pathname)
+            return {
+                url: imageUrl,
+                fileName: `${imageName}${imageExtension}`
+            }
+        }).toArray()
+
+        if (images.length > 0) {
+            images.forEach(image => {
+                mappedItem.content = mappedItem.content.replace(image.url, image.fileName)
+            })
+            mappedItem.images = images
+        }
+
+        return mappedItem
+    })
+
+    // Add Markdown conversion
+    items = items.map(item => {
+        item.markdownContent = turndownService.turndown(item.content)
+        return item
+    })
+
+    return items
+}
+
+const parseFeed = (file) => {
+    return new Promise((resolve, reject) => {
+        feedRead.parseString(file, undefined, (error, result) => {
+            if (error) {
+                reject(error)
+            } else {
+                resolve(result)
+            }
+        })
+    })
+}
+
+module.exports = { parse: parse }
